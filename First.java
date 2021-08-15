53c6066e0aa7c9aaee980e0d260209a22d1cd716
Mansi Bisht <84663893+mansibisht22@users.noreply.github.com>
2021-08-15 19:32:19 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args) {
+        //Taking input of two numbers
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
