c05cfca59c19f409a4fabdc98dec22d3747e9235
Mansi Bisht <84663893+mansibisht22@users.noreply.github.com>
2021-08-16 17:55:02 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        //Primitve Data Types in Java
+
+        int roll_no = 7;
+        float marks = 74.36f;
+        boolean happiness = true;
+        char name_initial = 'M';
+
+        System.out.println(marks);
+    }
+}
